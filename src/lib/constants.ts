35000773e1339ef2750fbe07ae30e817eff1f44b
Liam O'Kane <ldokane01@gmail.com>
2024-06-15 19:20:29 +1000

    update constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,8 +2,8 @@ import { GameState, Lobby } from "./types";
 
 export const DISCUSSION_TIME_STEP_SIZE = 15;
 export const MIN_DISCUSSION_TIME = 15;
-const DEFAULT_DISCUSSION_TIME = 60;
 export const MAX_DISCUSSION_TIME = 300;
+const DEFAULT_DISCUSSION_TIME = 60;
 
 export const COUNTDOWN_TIME = 10;
 
